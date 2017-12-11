cda465356b805d5ab0a39c54f3b4b1d8062d7924
Sebastian Häni <haeni.sebastian@gmail.com>
2017-12-11 21:02:12 +0100

    fix schema generation on my old MySQL server :(

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      // See https://laravel.com/docs/5.5/migrations#indexes under Index Lengths & MySQL / MariaDB
+      Schema::defaultStringLength(191);
     }
 
     /**
