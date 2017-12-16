14bb706501c47693b45974e6874c303928b092c3
Sebastian Häni <haeni.sebastian@gmail.com>
2017-12-16 23:22:03 +0100

    remove require-dev from composer install command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -evx # exit when command fails; print each line executed
+set -ex # exit when command fails; print each line executed
 
 cp -R api/ dist/api/
 
@@ -9,7 +9,7 @@ cd dist/
 find . -type f -name ".gitignore" -delete
 
 git init
-git add -A > /dev/null
+git add -A
 git commit -m "deploy"
 git remote add origin https://$GIT_USERNAME:$GIT_PASSWORD@thermoboard.sebastianhaeni.ch/plesk-git/thermoboard.git
 git push --force origin master
