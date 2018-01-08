fc2db21a197096cbc83d013711f60e6b03fd3721
Sebastian Häni <haeni.sebastian@gmail.com>
2018-01-08 11:34:50 +0100

    improve favorite display on dashboard

--- src/app/static/dashboard/dashboard.component.ts
+++ src/app/static/dashboard/dashboard.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ANIMATE_ON_ROUTE_ENTER } from '@app/core/animations/router.transition';
 import { FavoriteService } from '@app/core/favorite/favorite.service';
+import Favorite from '@app/core/favorite/Favorite';
 
 @Component({
   selector: 'thermo-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
   animateOnRouteEnter = ANIMATE_ON_ROUTE_ENTER;
+  favorites: Favorite[];
 
-  constructor(private favoriteService: FavoriteService) {
+  constructor(favoriteService: FavoriteService) {
+    this.favorites = favoriteService.getFavorites();
   }
-
-  ngOnInit() {
-  }
-
 }
